e8a8e6234f9c1c47f3f823f2ea605a82fec39b49
Mehmet Eren Yener <mehmeterenyener@gmail.com>
2017-02-02 01:36:34 +0300

    thumbify ctor created

---
+++ dist/thumbify.js
@@ -0,0 +1,18 @@
+/*
+ * Owl Carousel v1.0.0
+ * Copyright 2017 Eren Yener
+ */
+
+;(function($, window, document, undefined) {
+
+    function Thumbify(){
+
+    }
+
+    $.fn.thumbify = function(option) {
+
+    };
+
+    $.fn.thumbify.Constructor = Thumbify;
+
+})(window.jQuery, window, document);
