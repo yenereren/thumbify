d6a8a871e16a89dabe97babc7b27dd9922074df7
Mehmet Eren Yener <mehmeterenyener@gmail.com>
2017-02-02 02:52:38 +0300

    setup & log fuctions added

--- dist/thumbify.js
+++ dist/thumbify.js
@@ -5,12 +5,35 @@
 
 ;(function($, window, document, undefined) {
 
-    function Thumbify(){
+    function Thumbify(element, options){
+        this.log("thumbfy ctor init");
+        this.settings = null;
 
+        this.options = $.extend({}, Thumbify.Defaults, options);
+        this.$element = $(element);
+
+        this.setup();
     }
 
-    $.fn.thumbify = function(option) {
+    Thumbify.Defaults = {
+    };
 
+    Thumbify.prototype.setup = function() {
+
+    };
+
+    Thumbify.prototype.log = function(message) {
+        console.log("thumbify message -> " + message);
+    };
+
+
+    $.fn.thumbify = function(option) {
+        var args = Array.prototype.slice.call(arguments, 1);
+
+        return this.each(function() {
+            var $this = $(this);
+            new Thumbify(this, typeof option == 'object' && option);
+        });
     };
 
     $.fn.thumbify.Constructor = Thumbify;
