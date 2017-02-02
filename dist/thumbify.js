255b5b02d3116e35ef25c0a7646cbee8b8cb3eae
Mehmet Eren Yener <mehmeterenyener@gmail.com>
2017-02-02 03:36:02 +0300

    style registering

--- dist/thumbify.js
+++ dist/thumbify.js
@@ -6,26 +6,40 @@
 ;(function($, window, document, undefined) {
 
     function Thumbify(element, options){
-        this.log("thumbfy ctor init");
-        this.settings = null;
 
+        this.settings = null;
         this.options = $.extend({}, Thumbify.Defaults, options);
         this.$element = $(element);
+        this.$images = this.$element.find('img');
+
 
         this.setup();
     }
 
     Thumbify.Defaults = {
+        debugMode:true,
+        showNagivation:true,
+        containerClass:'thumbify-container',
+        imageClass:'thumbify-image'
     };
 
     Thumbify.prototype.setup = function() {
-
+        this.log('setup');
+        this.registerStyles();
     };
 
     Thumbify.prototype.log = function(message) {
-        console.log("thumbify message -> " + message);
+        if(this.options.debugMode){
+            console.log('thumbify message -> ' + message);
+        }
     };
 
+    Thumbify.prototype.registerStyles = function(){
+        var self =this;
+        this.log('registerStyles');
+        this.$element.addClass(this.options.containerClass);
+        this.$images.addClass(this.options.imageClass);
+    };
 
     $.fn.thumbify = function(option) {
         var args = Array.prototype.slice.call(arguments, 1);
